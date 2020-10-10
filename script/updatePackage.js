c4026203e13e5ebd29fb17c315c1e3ca7a094c4d
张琦 <qi.zhang@forceclouds.com>
2020-10-10 21:52:17 +0800

    update

--- script/updatePackage.js
+++ script/updatePackage.js
@@ -13,14 +13,15 @@ const PACKAGE_CONFIG = {
 
 const PACKAGE_SCRIPT = {};
 
-fs.readFile(resolve(__dirname, "../../package.json"), "utf8", (err, str) => {
+fs.readFile(resolve(__dirname, "../../../package.json"), "utf8", (err, str) => {
+  console.log("read package.json data", str);
   const data = JSON.parse(str);
 
   // * 修改package.json config和script
   data.config = Object.assign({}, data.config, PACKAGE_CONFIG);
 
   fs.writeFileSync(
-    resolve(__dirname, "../../package2.json"),
+    resolve(__dirname, "../../../package2.json"),
     JSON.stringify(data, "", "\t")
   );
 });
